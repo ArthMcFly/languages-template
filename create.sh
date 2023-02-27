ee808a3f401542e4fb569f58201e8cfe335d3700
ArthMcFly <102303972+ArthMcFly@users.noreply.github.com>
2023-02-27 19:54:03 -0300

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#variables
+file=($1)
+var=($2)
+C_way=(/home/arthur/programming/C_Scripts)
+Sh_way=(/home/arthur/programming/Shell_Scripts)
+Py_way=(/home/arthur/programming/Python_Scripts)
+
+#script
+if [ ${var} == -c ]; then
+	cp ${C_way}/template_c ${file}.c
+	nvim ${file}.c 
+fi
+if [ ${var} == -sh ]; then
+    cp ${Sh_way}/template_sh ${file}.sh
+	nvim ${file}.sh
+
+fi
+
+
+
