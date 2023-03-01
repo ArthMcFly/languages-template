3f0ac8fd87447382fb8cf46f7fd037398738fc9f
ArthMcFly <102303972+ArthMcFly@users.noreply.github.com>
2023-03-01 18:34:27 -0300

    Add files via upload

--- create.sh
+++ create.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 #variables
-file=($1)
+filename=($1)
 var=($2)
-C_way=(/home/arthur/programming/C_Scripts)
-Sh_way=(/home/arthur/programming/Shell_Scripts)
+temp_way=(/home/arthur/templates)
 
 #script
 if [ ${var} == -c ]; then
-	cp ${C_way}/template_c ${file}.c
-	nvim ${file}.c 
+	cp ${temp_way}/template_c ${filename}.c
+	nvim ${filename}.c 
 fi
 if [ ${var} == -sh ]; then
-    cp ${Sh_way}/template_sh ${file}.sh
-	nvim ${file}.sh
+    cp ${temp_way}/template_sh ${filename}.sh
+	chmod +x ${filename}.sh 
+	nvim ${filename}.sh
+
+fi
+if [ $var == -html ]; then
+	cp ${temp_way}/template_html ${filename}.html
+	nvim ${filename}.html
 
 fi
 
